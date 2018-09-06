cfc5b173fe26a3f3a98a9d2f9d7f881faeba434a
bliepp <comedyclub333.yt@googlemail.com>
2018-09-07 01:10:28 +0200

    Increase Efficiency

--- snow.py
+++ snow.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-#
+# a random snow animation
 #
 
 import pygame, math
@@ -16,13 +16,16 @@ class Snowflake():
             '__pos',
             '__vel',
             '__acc'
-    ] # non-dynamic attributes
+        ] # non-dynamic attributes
     
     def __init__(self, mass = 1):
         self.__rand = (random.random()*0.35)+0.65
         self.__mass = mass
         
-        self.__pos = array( [random.random()*(screen_size[0]+40)-20 , -5+self.__mass-5] )
+        self.__pos = array(
+                [random.random()*(screen_size[0]+40)-20 ,
+                    -5+self.__mass-5]
+            )
         self.__vel = array( [(random.random()-0.5)*0.3, 0.0] )
         self.__acc = array( [0.0, 0.5] )*self.__mass
     
@@ -33,8 +36,14 @@ class Snowflake():
     
     def render(self, on):
         self.update()
-        pygame.draw.circle(on, white, (int(self.__pos[0]), int(self.__pos[1])), int(5*self.__mass*self.__rand))
+        pygame.draw.circle(
+                on,
+                white,
+                (int(self.__pos[0]), int(self.__pos[1])),
+                int(5*self.__mass*self.__rand)
+            )
     
+    @property
     def off(self):
         if (self.__pos[1] > screen_size[1]+5*self.__mass or
                 self.__pos[0] < -30 or
@@ -43,17 +52,9 @@ class Snowflake():
         return False
 
 
-def toggle(var):
-    if var == True:
-        return False
-    elif var == False:
-        return True
-
 # setup
 info = pygame.display.Info()
 screen_size = (info.current_w,info.current_h)
-title = "Snowflakes!"
-mouse = False
 
 # colors
 black = (0,0,0)
@@ -61,37 +62,36 @@ white = (255,255,255)
 
 # init
 screen = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
-pygame.display.set_caption(title)
-pygame.mouse.set_visible(mouse)
+pygame.display.set_caption("Snowflakes!")
+pygame.mouse.set_visible(False)
 
 clock = pygame.time.Clock()
 
 # main loop
-running = True
-
-gen = True
+running = True # run program?
+gen = True # generate snow?
 snow = []
+
 while running:
-    screen.fill(black)
+    screen.fill(black) # redraw screen
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: running = False
-            if event.key == pygame.K_SPACE: gen = toggle(gen)
+            if event.key == pygame.K_SPACE: gen = not gen # toggle snow
     
     random_gen = random.random()
     if random_gen <= 0.35 and gen == True:
-        random_mass = 0.0
+        random_mass = random.random()
         while random_mass < 0.5: random_mass = random.random()
         snow.append( Snowflake(random_mass) )
         
-    #print(len(snow))
-    for flake in snow:
-        flake.render(screen)
+    for i in range( len(snow)-10 ): # don't delete all off screens,
+        if snow[i].off: del snow[i] # preserve the last ten
     
-    for i in range( len(snow)-10 ):
-        if snow[i].off(): del snow[i]
+    for flake in snow:
+        flake.render(screen) 
     
     pygame.display.update()
-    clock.tick(120)
+    clock.tick(120) # 120 fps
